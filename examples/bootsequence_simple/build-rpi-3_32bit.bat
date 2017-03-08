d7293af2c6d68a86b2246980ba3e948cc1ce1d83
hexoul <crosien@gmail.com>
2017-03-08 17:24:37 +0900

    Add macro for compiler name

--- examples/bootsequence_simple/build-rpi-3_32bit.bat
+++ examples/bootsequence_simple/build-rpi-3_32bit.bat
@@ -1,11 +1,12 @@
 set target=ex1
+set cc=arm-none-eabi
 
-arm-none-eabi-as start.s -o start.o
+%cc%-as start.s -o start.o
 
-arm-none-eabi-gcc -Wall -O2 -nostdlib -nostartfiles -ffreestanding -c %target%.c -o %target%.o
+%cc%-gcc -Wall -O2 -nostdlib -nostartfiles -ffreestanding -c %target%.c -o %target%.o
 
-arm-none-eabi-ld start.o %target%.o -T memmap -o %target%.elf
-arm-none-eabi-objdump -D %target%.elf > %target%.list
+%cc%-ld start.o %target%.o -T memmap -o %target%.elf
+%cc%-objdump -D %target%.elf > %target%.list
 
-arm-none-eabi-objcopy %target%.elf -O binary kernel7.img
-arm-none-eabi-objcopy %target%.elf -O ihex %target%.hex
+%cc%-objcopy %target%.elf -O binary kernel7.img
+%cc%-objcopy %target%.elf -O ihex %target%.hex
--- examples/bootsequence_with_gpio_led/build-rpi-3_32bit.bat
+++ examples/bootsequence_with_gpio_led/build-rpi-3_32bit.bat
@@ -1,11 +1,12 @@
 set target=ex2
+set cc=arm-none-eabi
 
-arm-none-eabi-as start.s mailbox.s led.s -o start.o
+%cc%-as start.s mailbox.s led.s -o start.o
 
-arm-none-eabi-gcc -Wall -O0 -nostdlib -nostartfiles -ffreestanding -c %target%.c -o %target%.o
+%cc%-gcc -Wall -O0 -nostdlib -nostartfiles -ffreestanding -c %target%.c -o %target%.o
 
-arm-none-eabi-ld start.o %target%.o -T memmap -o %target%.elf
-arm-none-eabi-objdump -D %target%.elf > %target%.list
+%cc%-ld start.o %target%.o -T memmap -o %target%.elf
+%cc%-objdump -D %target%.elf > %target%.list
 
-arm-none-eabi-objcopy %target%.elf -O binary kernel7.img
-arm-none-eabi-objcopy %target%.elf -O ihex %target%.hex
+%cc%-objcopy %target%.elf -O binary kernel7.img
+%cc%-objcopy %target%.elf -O ihex %target%.hex
