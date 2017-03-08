1b8c4628c679bd95ba8a81e775269e8a69329c8d
hexoul <crosien@gmail.com>
2017-03-08 15:52:41 +0900

    kernel8.img doesn't work apply macro on batch

--- examples/bootsequence_simple/build-rpi-3_32bit.bat
+++ examples/bootsequence_simple/build-rpi-3_32bit.bat
@@ -1,9 +1,11 @@
+set target=ex1
+
 arm-none-eabi-as start.s -o start.o
 
-arm-none-eabi-gcc -Wall -O2 -nostdlib -nostartfiles -ffreestanding -c ex1.c -o ex1.o
+arm-none-eabi-gcc -Wall -O2 -nostdlib -nostartfiles -ffreestanding -c %target%.c -o %target%.o
 
-arm-none-eabi-ld start.o ex1.o -T memmap -o ex1.elf
-arm-none-eabi-objdump -D ex1.elf > ex1.list
+arm-none-eabi-ld start.o %target%.o -T memmap -o %target%.elf
+arm-none-eabi-objdump -D %target%.elf > %target%.list
 
-arm-none-eabi-objcopy ex1.elf -O binary kernel8.img
-arm-none-eabi-objcopy ex1.elf -O ihex ex1.hex
+arm-none-eabi-objcopy %target%.elf -O binary kernel7.img
+arm-none-eabi-objcopy %target%.elf -O ihex %target%.hex
