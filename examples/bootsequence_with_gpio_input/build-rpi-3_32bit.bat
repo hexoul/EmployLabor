fd1cada18113b725d3e6761f9ff6fdaeb289d6a4
upoque <upoque@gmail.com>
2017-03-09 02:58:09 +0900

    added GPIO input methods + GPIO register mapping

---
+++ examples/bootsequence_with_gpio_input/build-rpi-3_32bit.bat
@@ -0,0 +1,12 @@
+set target=ex2
+set cc=arm-none-eabi
+
+%cc%-as start.s mailbox.s led.s -o start.o
+
+%cc%-gcc -Wall -O0 -nostdlib -nostartfiles -ffreestanding -c %target%.c -o %target%.o
+
+%cc%-ld start.o %target%.o -T memmap -o %target%.elf
+%cc%-objdump -D %target%.elf > %target%.list
+
+%cc%-objcopy %target%.elf -O binary kernel7.img
+%cc%-objcopy %target%.elf -O ihex %target%.hex
